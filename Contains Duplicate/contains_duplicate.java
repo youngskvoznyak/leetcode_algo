ffe8dba6d23a434dc358a4b96dcf7a6b0236704f
Max Salikhov <fanny2man@gmail.com>
2022-05-03 17:25:24 +0300

    add java solution

---
+++ Contains Duplicate/contains_duplicate.java
@@ -0,0 +1,9 @@
+public boolean containsDuplicate(int[] nums) {
+        Set<Integer> numbers = new HashSet<Integer>();
+        for (int num : nums) {
+                if (numbers.contains(num)) return true;
+                numbers.add(num);
+        }
+
+        return false;
+}
