71b126445c0ebaa205af3e9139bc2fecf1282415
Max Salikhov <fanny2man@gmail.com>
2022-09-22 09:12:38 +0300

    add golang solution

---
+++ Contains Duplicate/containDuplicate.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	nums := []int{}
+	fmt.Println(containsDuplicateOne(nums))
+}
+
+func containsDuplicateOne(nums []int) bool {
+	visit := make(map[int]bool)
+	for _, v := range nums {
+		if visit[v] {
+			return true
+		}
+		visit[v] = true
+	}
+	return false
+}
+
+func containsDuplicatTwo(nums []int) bool {
+	visit := make(map[int]struct{})
+	for _, v := range nums {
+		if _, ok := visit[v]; ok {
+			return true
+		}
+		visit[v] = struct{}{}
+	}
+	return false
+}
