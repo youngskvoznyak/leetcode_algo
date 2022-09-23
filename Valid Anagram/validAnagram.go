6df0482f13cdb0524a20d19f02eacd3eba1f465b
Max Salikhov <fanny2man@gmail.com>
2022-09-23 17:36:53 +0300

    add golang solution

---
+++ Valid Anagram/validAnagram.go
@@ -0,0 +1,22 @@
+package main
+
+func main() {
+
+}
+
+func validAnagram(s, t string) bool {
+	if len(s) != len(t) {
+		return false
+	}
+	var m [26]int
+	for i := 0; i < len(s); i++ {
+		m[s[i]-'a']++
+		m[t[i]-'a']--
+	}
+	for _, v := range m {
+		if v != 0 {
+			return false
+		}
+	}
+	return true
+}
