6b4fec1c166d198fb1c415ef8ce47365c11d463c
Maxim <fanny2man@gmail.com>
2022-10-03 10:27:51 +0300

    add golang solution

---
+++ Group Anagrams/groupAnagram.go
@@ -0,0 +1,27 @@
+package main
+
+func main() {
+	strs := []string{"eat", "tea", "tan", "ate", "nat", "bat"}
+	groupAnagram(strs)
+}
+
+func groupAnagram(strs []string) [][]string {
+
+	m := make(map[[26]int][]string)
+
+	for _, s := range strs {
+		var freq [26]int
+
+		for _, c := range s {
+			freq[c-'a']++
+		}
+		m[freq] = append(m[freq], s)
+	}
+
+	var groups [][]string
+	for _, v := range m {
+		groups = append(groups, v)
+	}
+	return groups
+
+}
