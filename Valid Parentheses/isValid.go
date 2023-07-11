04f322c697a9093500bf40cf5cb99be3da32466d
Max Salikhov <fanny2man@gmail.com>
2023-07-11 17:39:53 +0300

    add solution

---
+++ Valid Parentheses/isValid.go
@@ -0,0 +1,33 @@
+package main
+
+func main() {
+
+}
+
+func isValid(s string) bool {
+	pairs := map[byte]byte{
+		'}': '{',
+		']': '[',
+		')': '(',
+	}
+
+	stack := make([]byte, 0)
+
+	for _, char := range []byte(s) {
+		pair, ok := pairs[char]
+		if !ok {
+			stack = append(stack, char)
+			continue
+		}
+
+		if len(stack) == 0 {
+			return false
+		}
+
+		if stack[len(stack)-1] != pair {
+			return false
+		}
+		stack = stack[:len(stack)-1]
+	}
+	return len(stack) == 0
+}
