dd930eb82a2f89796718fcb6404ab7d406cf8b91
Max Salikhov <fanny2man@gmail.com>
2023-07-25 13:58:30 +0300

    add soluyion

---
+++ Validate Binary Search Tree/isValidBST.go
@@ -0,0 +1,31 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func main() {
+
+}
+
+func isValidBST(root *TreeNode) bool {
+	return isValid(root, nil, nil)
+}
+
+func isValid(root, min, max *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+
+	if min != nil && root.Val <= min.Val {
+		return false
+	}
+
+	if max != nil && root.Val >= max.Val {
+		return false
+	}
+
+	return isValid(root.Left, min, root) && isValid(root.Right, root, max)
+}
