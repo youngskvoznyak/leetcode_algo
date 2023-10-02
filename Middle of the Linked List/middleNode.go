4f8cc864036e2a0d2bfad0d2eb560bdb78446824
Max Salikhov <fanny2man@gmail.com>
2023-10-02 10:54:52 +0300

    add solution

--- Middle of the Linked List/middleNode.go
+++ Middle of the Linked List/middleNode.go
@@ -12,6 +12,7 @@ type ListNode struct {
 func middleNode(head *ListNode) *ListNode {
 	slow, fast := head, head
 
+	// пока быстрый указатель существует и ему есть куда идти
 	for fast != nil && fast.Next != nil {
 		slow = slow.Next
 		fast = fast.Next.Next
